41e8a0b39b5ec67b66893c1d43e4f5152c91e16f
Jmhram <44422541+Jmhram@users.noreply.github.com>
2019-05-24 13:13:56 +0800

    frame::3.5

---
+++ Hail Of Bullets/src/HOB/model/Direction.java
@@ -0,0 +1,22 @@
+package HOB.model;
+
+public enum Direction //对方向的枚举
+{
+    /**
+     * 上
+     */
+    up,
+    /**
+     * 右
+     */
+    right,
+    /**
+     * 下
+     */
+    down,
+    /**
+     * 左
+     */
+    left,
+}
+
